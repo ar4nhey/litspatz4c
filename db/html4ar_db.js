a6ff46e6e28d1256e0120d276dabea4b6b3593e9
Engelbert Niehaus <niehausbert@gmail.com>
2025-07-09 16:50:26 +0200

    Add files via upload

---
+++ db/html4ar_db.js
@@ -0,0 +1,71 @@
+vDataJSON.html4ar = `
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>AR.js - ___TITLE___ - ___MARKER___ </title>
+  <script src="___DIR___/js/aframe-1.2.0.js"></script>
+  <script src="___DIR___/js/aframe-ar.js"> </script>
+  <script src="___DIR___/js/aframe-1.7.0-extras.js"></script>
+  <script src="___DIR___/js/aframe-object-mover.js"></script>
+  <script>
+        // Include the ObjectMover class
+        // Create an instance of ObjectMover for the box
+        const ___MOVEID___Mover = new AframeMover('___MOVEID___','ar');
+		// Time stamps alternative hide/show
+		// ___MOVEID___Mover.setVisibleTimes([ 0.1 , 0.3 , 0.5 , 0.7 , 0.9]);
+        // 0.1 = Hide after 10% of animation time
+        // 0.3 = Show after 30% of animation time
+        // 0.5 = Hide after 50% of animation time
+        // ...
+        // All positions are 3D coordinates (x,y,z)
+        // x: -left/+right
+        // y: -down/+up
+        // z: -rear/+front
+        // (0,0,0) is the marker position
+        ___MOVEID___Mover.setStartPosition(__STARTPOS4K__);
+        ___MOVEID___Mover.setMiddle1Position(__MIDDLE1POS4K__);
+        ___MOVEID___Mover.setMiddle2Position(__MIDDLE1POS4K__);
+        ___MOVEID___Mover.setEndPosition(__ENDPOS4K__);
+        ___MOVEID___Mover.setLoop(___LOOPBOOL___);
+        // Set start and end rotations - no rotation
+        ___MOVEID___Mover.setStartRotation(___STARTROT4K___);
+        ___MOVEID___Mover.setEndRotation(___ENDROT4K___);
+
+        // Set duration
+        ___MOVEID___Mover.setDuration(___DURATION___); // i.e. ___DURATION___/1000 seconds
+
+        // Start the movement
+        ___MOVEID___Mover.startMovement();
+  </script>
+
+</head>
+<body style="margin : 0px; overflow: hidden;">
+  <a-scene embedded arjs>
+  <a-marker preset="___MARKER___">
+    <a-entity ___MOVEID___ position="___STARTPOS___">
+      <a-entity
+        position="___STARTPOS___"
+        rotation="___STARTROT___"
+        scale="__SCALE__ __SCALE__ __SCALE__"
+        animation-mixer="loop: repeat"
+        gltf-model="___DIR___/___GLB___" >
+      </a-entity>
+    </a-entity>
+  </a-marker>
+  <a-light type="ambient" color="#FFF" light=""></a-light>
+  <a-light type="point" color="#fff" position="0 3 0" light=""></a-light>
+  <a-light type="point" color="#fff" position="3 3 0" light=""></a-light>
+  <a-light type="point" color="#fff" position="-3 3 0" light=""></a-light>
+  <a-light type="point" color="#fff" position="3 3 -3" light=""></a-light>
+  <a-light type="point" color="#fff" position="-3 3 -3" light=""></a-light>
+  <a-light type="point" color="#fff" position="1.5 3 0" light=""></a-light>
+
+    <!-- Define the camera -->
+    <a-entity camera></a-entity>
+  </a-scene>
+</body>
+</html>
+
+`;
---
+++ db/html4argeo_db.js
@@ -0,0 +1,78 @@
+vDataJSON.html4argeo = `
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <title>AR.js - ___TITLE___ - Geolocation </title>
+        <script src="https://aframe.io/releases/1.0.4/aframe.min.js"></script>
+        <script src="https://unpkg.com/aframe-look-at-component@0.8.0/dist/aframe-look-at-component.min.js"></script>
+        <script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar-nft.js"></script>
+        <script src="https://raw.githack.com/donmccurdy/aframe-extras/master/dist/aframe-extras.loaders.min.js"></script>
+        <!--
+        <script src="___DIR___/js/aframe-1.2.0.js"></script>
+        <script src="___DIR___/js/aframe-ar.js"> </script>
+        <script src="___DIR___/js/aframe-1.7.0-extras.js"></script>
+        -->
+        <script src="___DIR___/js/aframe-object-mover.js"></script>
+        <script>
+              // Include the ObjectMover class
+              // Create an instance of ObjectMover for the box
+              const ___MOVEID___Mover = new AframeMover('___MOVEID___','ar');
+      		    // Time stamps alternative hide/show
+      		    // ___MOVEID___Mover.setVisibleTimes([ 0.1 , 0.3 , 0.5 , 0.7 , 0.9]);
+              // 0.1 = Hide after 10% of animation time
+              // 0.3 = Show after 30% of animation time
+              // 0.5 = Hide after 50% of animation time
+              // ...
+              // All positions are 3D coordinates (x,y,z)
+              // x: -left/+right
+              // y: -down/+up
+              // z: -rear/+front
+              // (0,0,0) is the marker position
+              ___MOVEID___Mover.setStartPosition(__STARTPOS4K__);
+              ___MOVEID___Mover.setMiddle1Position(__MIDDLE1POS4K__);
+              ___MOVEID___Mover.setMiddle2Position(__MIDDLE1POS4K__);
+              ___MOVEID___Mover.setEndPosition(__ENDPOS4K__);
+              ___MOVEID___Mover.setLoop(___LOOPBOOL___);
+              // Set start and end rotations - no rotation
+              ___MOVEID___Mover.setStartRotation(___STARTROT4K___);
+              ___MOVEID___Mover.setEndRotation(___ENDROT4K___);
+
+              // Set duration
+              ___MOVEID___Mover.setDuration(___DURATION___); // i.e. ___DURATION___/1000 seconds
+
+              // Start the movement
+              ___MOVEID___Mover.startMovement();
+        </script>
+      </head>
+
+    <body style="margin: 0; overflow: hidden;">
+        <a-scene
+            renderer="logarithmicDepthBuffer: true;"
+            embedded
+            loading-screen="enabled: false;"
+            arjs="sourceType: webcam; debugUIEnabled: false;"
+        >
+            <a-assets>
+                <a-asset-item
+                    id="animated-asset"
+                    src="___DIR___/___GLB___"
+                ></a-asset-item>
+            </a-assets>
+            <a-entity ___MOVEID___ position="0 0 0" >
+                <a-entity
+                    look-at="[gps-camera]"
+                    animation-mixer="loop: repeat"
+                    gltf-model="#animated-asset"
+                    scale="__SCALE__ __SCALE__ __SCALE__"
+                    gps-entity-place="latitude: ___LAT___; longitude: ___LONG___;"
+                ></a-entity>
+            </a-entity>
+
+            <a-camera gps-camera rotation-reader></a-camera>
+        </a-scene>
+    </body>
+</html>
+
+`;
