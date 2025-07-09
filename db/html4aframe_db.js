b4c8db61167264675e1bc6cc3206dcbb5499511c
Engelbert Niehaus <niehausbert@gmail.com>
2025-07-09 16:50:03 +0200

    Create html4aframe_db.js

---
+++ db/html4aframe_db.js
@@ -0,0 +1,68 @@
+vDataJSON.html4aframe = `
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>___TITLE___</title>
+    <script src="___DIR___/js/aframe-1.7.0.js"></script>
+    <script src="___DIR___/js/aframe-extras.js"></script>
+    <script src="___DIR___/js/aframe-object-mover.js"></script>
+	<script>
+        // Include the ObjectMover class
+        // Create an instance of ObjectMover for the box
+        const ___MOVEID___Mover = new AframeMover('___MOVEID___','ar');
+		// Time stamps alternative hide/show
+		// ___MOVEID___Mover.setVisibleTimes([ 0.1 , 0.3 , 0.5 , 0.7 , 0.9]);
+        // 0.1 = Hide after 10% of animation time
+        // 0.3 = Show after 30% of animation time
+        // 0.5 = Hide after 50% of animation time
+        // ...
+        // All positions are 3D coordinates (x,y,z)
+        // x: -left/+right
+        // y: -down/+up
+        // z: -rear/+front
+        // (0,0,0) is the marker position
+        ___MOVEID___Mover.setStartPosition(__STARTPOS4K__);
+        ___MOVEID___Mover.setMiddle1Position(__MIDDLE1POS4K__);
+        ___MOVEID___Mover.setMiddle2Position(__MIDDLE1POS4K__);
+        ___MOVEID___Mover.setEndPosition(__ENDPOS4K__);
+        ___MOVEID___Mover.setLoop(___LOOPBOOL___);
+        // Set start and end rotations - no rotation
+        ___MOVEID___Mover.setStartRotation(___STARTROT4K___);
+        ___MOVEID___Mover.setEndRotation(___ENDROT4K___);
+
+        // Set duration
+        ___MOVEID___Mover.setDuration(___DURATION___); // i.e. ___DURATION___/1000 seconds
+
+        // Start the movement
+        ___MOVEID___Mover.startMovement();
+	</script>
+
+</head>
+<body>
+  <a-scene inspector="" keyboard-shortcuts="" screenshot="" vr-mode-ui="" device-orientation-permission-ui="" visible="">
+    <!-- Load the 3D model -->
+    <a-assets>
+      <a-asset-item id="my3dmodel" src="___DIR___/___GLB___"></a-asset-item>
+      <img id="sky" src="___DIR___/___SKY___">
+    </a-assets>
+
+    <!-- Add the 3D model to the scene -->
+    <a-entity gltf-model="#my3dmodel" ___MOVERID___
+      scale="___SCALE___ ___SCALE___ ___SCALE___"
+      position="STARTPOS"
+      rotation="STARTROT"
+      animation-mixer></a-entity>
+
+    <!-- Add some basic lighting -->
+    <a-light type="ambient" color="#FFF" light=""></a-light>
+    <a-light type="point" color="#fff" position="___CAMPOS___" light=""></a-light>
+	  <a-sky src="#sky"></a-sky>
+    <!-- Add a camera -->
+    <a-camera position="0 2 -5" camera="" rotation="0 0 0" look-controls="" wasd-controls="" data-aframe-inspector-original-camera=""></a-camera>
+  <div class="a-loader-title" style="display: none;">___TITLE___</div></a-scene>
+</body>
+</html>
+
+`;
